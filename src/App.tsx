8f276dd1d20746ba4cd4d9ddfa82e88d7cb5566a
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2026-03-20 15:11:42 +0100

    commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  useState([{ id: 1, omschrijving: 'leerling' }, { id: 2, omschrijving: 'leerkracht' }, { id: 3, omschrijving: 'administratief medewerker' },])
-
-  return (
-    <>
-
-    </>
-  )
+  useState([
+    { id: 1, omschrijving: "leerling" },
+    { id: 2, omschrijving: "leerkracht" },
+    { id: 3, omschrijving: "administratief medewerker" },
+  ]);
+  // hallo test
+  return <></>;
 }
 
-export default App
+export default App;
